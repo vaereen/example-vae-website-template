26f3f64739de4ec18ade81765d471b059ed14f9c
vaereen <raziqsufi2006@gmail.com>
2026-09-05 22:13:19 +0800

    Update config.js

--- config.js
+++ config.js
@@ -17,7 +17,7 @@
  */
 
 /* ---------- THEME SELECTION ---------- */
-const THEME = "cyber"; // Change this to "cyber", "luxe", or "artisan"
+const THEME = "luxe"; // Change this to "cyber", "luxe", or "artisan"
 
 /* ---------- NICHE SELECTION ---------- */
 const NICHE = "services"; // Change this to "services" or "food"
